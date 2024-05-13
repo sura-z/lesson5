f7793b7f52c5964ccd4cbcb5382299159c45df1d
sura-z <169043346+sura-z@users.noreply.github.com>
2024-05-13 15:23:25 +0300

    Add files via upload

---
+++ module1hand.py
@@ -0,0 +1,25 @@
+grades = [5, 3, 3, 5, 4]
+grades1 = [2, 2, 2, 3]
+grades2 = [4, 5, 5, 2]
+grades3 = [4, 4, 3]
+grades4 = [5, 5, 5, 4, 5]
+average = sum(grades) / len(grades), sum(grades1) / len(grades1), sum(grades2) / len(grades2), sum(grades3) / len(grades3), sum(grades4) / len(grades4)
+print(average)
+
+
+students = 'Aaron'
+students1 = 'Bilbo'
+students2 = 'Johnny'
+students3 = 'Khendrik'
+students4 = 'Steve'
+
+
+
+
+
+
+
+
+
+
+
